e760d72d1659b73b687eab7c491c7bbd3846bf7a
Almudena Calderón <1797322@alu.murciaeduca.es>
2023-06-16 17:03:19 +0200

    algunos cambios de diseño y eliminacion archivos sin uso

--- src/components/Landing/Footer/Footer.vue
+++ src/components/Landing/Footer/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-    <footer class="footer mt-auto py-3 activo">
+    <footer class="footer mt-auto py-3 activo fondo">
         <div class="container">
             <p class="text-center text-white lead">Copyright ©
                 MyHealthFirst.
@@ -13,4 +13,16 @@
 </script>
 
 <style lang="scss" scoped>
+.fondo {
+    background-image: url(../../../images/counter-bg.jpg);
+}
+.footer {
+  height: 150px;
+  width: 100%;
+  background-size: cover;
+  background-position: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
 </style>
